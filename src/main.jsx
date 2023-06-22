8225bdf7aacf8feb3d3ddcc195d59c69948c39bd
shivam birla <shivambirla497@gmail.com>
2023-06-22 11:47:19 +0530

    make a folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
 import './index.css'
-import Movie from './Movie-app'
+import Movie from './components/Movie/Movie'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <App /> */}
+   
     <Movie />
   </React.StrictMode>,
 )
